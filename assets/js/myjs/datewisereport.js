13f32c9d0c3df12bf5fdf6d133b74be7b8a1a14f
Sagar Morvadiya <morvadiyasagar99@gmail.com>
2020-07-18 17:46:13 +0530

    "changes of rg12  reort and date wise report"

--- assets/js/myjs/datewisereport.js
+++ assets/js/myjs/datewisereport.js
@@ -146,12 +146,16 @@ $(document).ready(function() {
     function Export1() {
         html2canvas($('#file_info')[0], {
             onrendered: function(canvas) {
+
+
                 var data = canvas.toDataURL();
                 var docDefinition = {
+                    pageOrientation: 'landscape',
                     content: [{
                         image: data,
-                        width: 500
-                    }]
+                        width: 750
+                    }],
+
                 };
                 pdfMake.createPdf(docDefinition).download("Table.pdf");
             }
